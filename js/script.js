2293b2314af75e2a6b23a21efb5c57f194de816c
Acercandro21 <davisalejandrooq@gmail.com>
2025-11-11 16:25:32 -0600

    ft:Se agrega ejemplo de eventos

--- js/script.js
+++ js/script.js
@@ -1,7 +1,3 @@
-var x = 10;
-var x = 100;
-console.log(x); 
-
 //Iteraciones 
 
 const cielo = document.getElementById("cielo");
@@ -9,3 +5,31 @@ const cielo = document.getElementById("cielo");
 for(let i=0; i<5; i++){
     cielo.innerHTML += "⭐";
 }
+
+// eventos 
+const cielo2 = document.getElementById("cielo2");
+
+const btnEstrella = document.getElementById("btnEstrella");
+
+btnEstrella.addEventListener("click" ,()=> {
+    cielo2.innerHTML += "⭐";
+})
+
+const btnPlaneta = document.getElementById("btnPlaneta");
+btnPlaneta.addEventListener("dblclick" ,()=>{
+    cielo2.innerHTML +="🪐";
+})
+
+const inputEmoji = document.getElementById("inputEmoji");
+
+const btnAgregarEmoji = document.getElementById("btnAgregarEmoji");
+
+btnAgregarEmoji.addEventListener("click", ()=>{
+    var emoji = inputEmoji.value.trim();
+    if (emoji) cielo.innerHTML += emoji;
+    inputEmoji.value="";
+})
+
+inputEmoji.addEventListener("keydown", e =>{
+    if(e.key==="Escape") cielo.innerHTML="";
+})
